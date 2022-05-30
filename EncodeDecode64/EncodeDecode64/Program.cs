2a2e4d4adb8236de0236fd3454e912778b9208f1
ahsia1 <allenhsia1@gmail.com>
2022-05-29 22:52:59 -0700

    Add: -Code snippet example for Encode Decode base 64 to reuse for password encryption purposes in passwords for web applications

---
+++ EncodeDecode64/EncodeDecode64/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EncodeDecode64
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string passText = "This is a password";
+
+            Console.WriteLine("Begin Encode Process");
+            Console.WriteLine("This is your password: {0}", passText);
+            var convertToBytes = System.Text.Encoding.UTF8.GetBytes(passText);
+            var encoded = System.Convert.ToBase64String(convertToBytes);
+            Console.WriteLine("This is your encoded password: {0}", encoded);
+            Console.WriteLine("End Encode Process");
+
+            Console.WriteLine("Begin Decode Process");
+            var convertFromBytes = System.Convert.FromBase64String(encoded);
+            Console.WriteLine("This is your Converted from base64 bytes, {0}", string.Join(", ", convertFromBytes));
+            var decoded = System.Text.Encoding.UTF8.GetString(convertFromBytes);
+            Console.WriteLine("This is your password: {0}", decoded);
+            Console.WriteLine("End decode process");
+        }
+    }
+}
